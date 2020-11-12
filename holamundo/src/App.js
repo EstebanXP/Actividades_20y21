cf0a719d7ea29048ec5fcd4e73049dba17a95970
Esteban Xelhuantzi <a324249@uach.mx>
2020-11-12 10:41:11 -0700

    Act20 y 21

---
+++ holamundo/src/App.js
@@ -0,0 +1,16 @@
+import  Parrafo from './components/Parrafo'
+import Contador from './components/Contador'
+import User from './components/User'
+
+function App() {
+  return (
+    <div >
+      <h1>Hola mundo</h1>
+      <Parrafo></Parrafo> 
+      <Contador></Contador>
+      <User></User>
+    </div>
+  );
+}
+
+export default App;
